273d67d3b53790697b24863d00a199c76df4a0c8
ナイアン <naya.kmb1520@gmail.com>
2026-04-29 17:03:54 -0300

    Add files via upload

---
+++ Projeto_Flor-de-lótus/salao/js/dados.js
@@ -0,0 +1,8 @@
+// Dados do cardápio/serviços do cliente.
+// Edite este arquivo para alterar os itens exibidos no site.
+const ITENS = [
+  { nome: "Corte Feminino", descricao: "Descrição breve do item.", preco: "R$ 10,00" },
+  { nome: "Coloração", descricao: "Descrição breve do item.", preco: "R$ 15,00" },
+  { nome: "Hidratação", descricao: "Descrição breve do item.", preco: "R$ 20,00" },
+  { nome: "Maquiagem Social", descricao: "Descrição breve do item.", preco: "R$ 25,00" }
+];
