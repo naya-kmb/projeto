458f51fad71a2a9a6451afbdd39858443f4ec545
ナイアン <naya.kmb1520@gmail.com>
2026-05-13 17:01:51 -0300

    Add files via upload

--- Projeto_Flor-de-lótus/salao/js/dados.js
+++ Projeto_Flor-de-lótus/salao/js/dados.js
@@ -1,8 +1,19 @@
 // Dados do cardápio/serviços do cliente.
 // Edite este arquivo para alterar os itens exibidos no site.
 const ITENS = [
-  { name: "corte feminino", description: "Descrição breve do item.", price: "R$ 10,00" },
-  { name: "Coloração", description: "Descrição breve do item.", price: "R$ 15,00" },
-  { name: "Hidratação", description: "Descrição breve do item.", price: "R$ 20,00" },
-  { name: "Maquiagem Social", description: "Descrição breve do item.", price: "R$ 25,00" }
+  { name: "hair cut", description: "Descrição breve do item.", price: "R$ 10,00" },
+  { name: "hair paint", description: "Descrição breve do item.", price: "R$ 15,00" },
+  { name: "hair hydration", description: "Descrição breve do item.", price: "R$ 20,00" },
+  { name: "lymphatic massage", description: "Descrição breve do item.", price: "R$ 20,00" },
+  { name: "waxing", description: "Descrição breve do item.", price: "R$ 20,00" },
+  { name: "laser depilation", description: "Descrição breve do item.", price: "R$ 20,00" },
+  { name: "fiber nail", description: "Descrição breve do item.", price: "R$ 20,00" },
+  { name: "nail extension", description: "Descrição breve do item.", price: "R$ 20,00" },
+  { name: "classic nail polish", description: "Descrição breve do item.", price: "R$ 20,00" },
+  { name: "body massage", description: "Descrição breve do item.", price: "R$ 20,00" },
+  { name: "skin cleansing", description: "Descrição breve do item.", price: "R$ 20,00" },
+  { name: "bride's day", description: "Descrição breve do item.", price: "R$ 20,00" },
+  { name: "eyelash extension", description: "Descrição breve do item.", price: "R$ 20,00" },
+  { name: "eyebrow design", description: "Descrição breve do item.", price: "R$ 20,00" },
+  { name: "Social make", description: "Descrição breve do item.", price: "R$ 25,00" }
 ];
---
+++ Projeto_Flor-de-lótus/salao/js/menu.js
@@ -0,0 +1,12 @@
+<script>
+        const mobileMenu = document.getElementById('mobile-menu');
+const navList = document.getElementById('nav-list');
+
+mobileMenu.addEventListener('click', () => {
+    // Alterna a classe 'active' para mostrar/esconder o menu
+    navList.classList.toggle('active');
+    
+    // Opcional: Animação simples no botão hambúrguer
+    mobileMenu.classList.toggle('is-active');
+});
+    </script>
